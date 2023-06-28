c0676fe1cac15a45951e79630fc2b66f8b7cfcb8
Cesar Augusto <ces.augusto108@gmail.com>
2023-06-28 08:59:13 -0300

    Initial project setup

---
+++ tqi-desafio-poo-dio/src/main/kotlin/augusto108/ces/bootcamptracker/Bootcamptracker.kt
@@ -0,0 +1,11 @@
+package augusto108.ces.bootcamptracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Bootcamptracker
+
+fun main() {
+    runApplication<Bootcamptracker>()
+}
