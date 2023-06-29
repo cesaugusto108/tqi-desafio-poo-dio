28ab7af69c013d1f9840ef2f5eac58b4423eea7d
Cesar Augusto <ces.augusto108@gmail.com>
2023-06-28 21:24:40 -0300

    Application profiles config files

--- tqi-desafio-poo-dio/src/main/kotlin/augusto108/ces/bootcamptracker/BootcampTracker.kt
+++ tqi-desafio-poo-dio/src/main/kotlin/augusto108/ces/bootcamptracker/BootcampTracker.kt
@@ -2,8 +2,10 @@ package augusto108.ces.bootcamptracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
 
 @SpringBootApplication
+@Profile("dev")
 class Bootcamptracker
 
 fun main() {
