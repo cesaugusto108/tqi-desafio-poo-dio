181b000ddb991d03c39ede6668e05dee3e5175c8
Cesar Augusto <ces.augusto108@gmail.com>
2023-07-13 21:07:11 -0300

    Value object Name immutable fields

--- tqi-desafio-poo-dio/src/main/kotlin/augusto108/ces/bootcamptracker/entities/Name.kt
+++ tqi-desafio-poo-dio/src/main/kotlin/augusto108/ces/bootcamptracker/entities/Name.kt
@@ -5,9 +5,9 @@ import jakarta.persistence.Embeddable
 
 @Embeddable
 class Name(
-    @Column(name = "first_name", nullable = false, length = 20) var firstName: String = "",
-    @Column(name = "middle_name", nullable = false, length = 20) var middleName: String = "",
-    @Column(name = "last_name", nullable = false, length = 20) var lastName: String = ""
+    @Column(name = "first_name", nullable = false, length = 20) val firstName: String = "",
+    @Column(name = "middle_name", nullable = false, length = 20) val middleName: String = "",
+    @Column(name = "last_name", nullable = false, length = 20) val lastName: String = ""
 ) {
     override fun toString(): String {
         return if (middleName == "") "$firstName $lastName" else "$firstName $middleName $lastName"
