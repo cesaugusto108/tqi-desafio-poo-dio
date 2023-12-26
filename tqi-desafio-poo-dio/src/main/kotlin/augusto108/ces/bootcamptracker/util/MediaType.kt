8a7246ea880f790c515395ad35c3a8a4e81efd35
Cesar Augusto <cesaraugusto@skiff.com>
2023-12-26 19:54:59 -0300

    MediaType object refactoring

--- tqi-desafio-poo-dio/src/main/kotlin/augusto108/ces/bootcamptracker/util/MediaType.kt
+++ tqi-desafio-poo-dio/src/main/kotlin/augusto108/ces/bootcamptracker/util/MediaType.kt
@@ -3,6 +3,7 @@ package augusto108.ces.bootcamptracker.util
 import org.springframework.http.MediaType
 
 object MediaType {
+
     const val JSON: String = MediaType.APPLICATION_JSON_VALUE
     const val YAML = "application/x-yaml"
 }
